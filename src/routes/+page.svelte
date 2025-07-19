65725796f28be3919dfa300d5d784f47c3b38b04
Dan <danielpearce1221@hotmail.com>
2025-07-19 23:33:04 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,9 +31,9 @@
           <Avatar.Image src="/pfp.png" alt="@matyii" />
           <Avatar.Fallback>MK</Avatar.Fallback>
         </Avatar.Root>
-        <Card.Title>Kristóf Mátyás</Card.Title>
-        <Card.Description class="text-neutral-300">20 year old IT technician, hobby coder</Card.Description>
-        <Card.Description class="text-neutral-300">server administration/management, coding</Card.Description>
+        <Card.Title>Daniel Pearce</Card.Title>
+        <Card.Description class="text-neutral-300">24 year old, system integration specialist, coder, hobbyist</Card.Description>
+        <Card.Description class="text-neutral-300">server integration, networking, gaming</Card.Description>
       </Card.Header>
       <Card.Content class="flex justify-center gap-2">
         <Tooltip.Provider>
@@ -42,7 +42,7 @@
               <Button
                 variant="default"
                 size="icon"
-                href="https://github.com/matyii"
+                href="https://github.com/danymo1221/"
                 target="_blank"
                 aria-label="GitHub profile"
               >
@@ -59,9 +59,9 @@
               <Button
                 variant="default"
                 size="icon"
-                href="mailto:matyas@matyas.services"
+                href="mailto:me@daniels-lab.com"
                 target="_blank"
-                aria-label="Send email to matyas@matyas.services"
+                aria-label="Send email to me@daniels-lab.com"
               >
                 <Mail />
               </Button>
@@ -75,7 +75,7 @@
               <Button
                 variant="default"
                 size="icon"
-                href="https://www.linkedin.com/in/itsmatyii/"
+                href="https://www.linkedin.com/in/danielpearce01/"
                 target="_blank"
                 aria-label="Open my Linkedin page"
               >
@@ -94,7 +94,7 @@
               <Button
                 variant="default"
                 size="icon"
-                href="https://status.itsmatyii.dev"
+                href="https://status.daniels-lab.com"
                 target="_blank"
                 aria-label="Check status of services"
               >
@@ -111,8 +111,8 @@
 
     <Card.Root class="w-full max-w-sm shadow-md flex flex-col h-full bg-white/10 backdrop-blur-2xl border border-white/30 rounded-xl text-white">
       <Card.Header class="flex flex-col items-center justify-center space-y-2 text-center">
-        <Card.Title>itsmatyii network</Card.Title>
-        <Card.Description class="text-neutral-700 dark:text-neutral-300">services, tools, and websites i manage</Card.Description>
+        <Card.Title>The Daniels-Lab Net 🤖</Card.Title>
+        <Card.Description class="text-neutral-700 dark:text-neutral-300">Core systems, projects & more</Card.Description>
       </Card.Header>
       <Card.Content class="flex-1 flex flex-col gap-4 justify-between">
         <ScrollArea class="flex flex-col w-full max-h-56 p-1">
@@ -126,12 +126,12 @@
           >
             <img src="https://selfhostguides.diy/shg.png" alt="selfhostguides.diy" class="h-10 object-cover rounded-md" />
             <div class="flex flex-col items-start">
-              <span class="font-semibold text-lg">selfhostguides.diy</span>
+              <span class="font-semibold text-lg">input1 here</span>
               <span class="text-neutral-700 dark:text-neutral-300 text-sm">guides and resources from the selfhosting community</span>
             </div>
             </a>
             <a
-            href="https://matyas.services"
+            href="https://mail.daniels-lab.com"
             target="_blank"
             rel="noopener noreferrer"
             class="flex items-center gap-4 p-4 rounded-xl shadow cursor-pointer w-full transition-all border border-white/30 bg-white/10 backdrop-blur-xl hover:shadow-lg hover:bg-white/20"
@@ -139,8 +139,8 @@
           >
             <img src="https://matyas.services/favicon.png" alt="Status" class="w-12 h-12 object-cover rounded-md" />
             <div class="flex flex-col items-start">
-              <span class="font-semibold text-lg">matyas.services</span>
-              <span class="text-neutral-700 dark:text-neutral-300 text-sm">offering server management, web development (soon)</span>
+              <span class="font-semibold text-lg">Daniels Lab Mailer ✉</span>
+              <span class="text-neutral-700 dark:text-neutral-300 text-sm">Mailing System for my network </span>
             </div>
           </a>
             <!-- <a
